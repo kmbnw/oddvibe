bd77ec43aa190c17927c18afc44645be11fe3214
Krysta Bouzek <nwkrystab@gmail.com>
2017-06-04 12:19:01 -0700

    New tests

---
+++ cpp/test/rtree_test.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2016 Krysta M Bouzek
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <unordered_map>
+#include <cmath>
+#include <vector>
+#include "rtree.h"
+#include "rtree_test.h"
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+CPPUNIT_TEST_SUITE_REGISTRATION(oddvibe::RTreeTest);
+
+namespace oddvibe {
+
+    void RTreeTest::setUp() {
+    }
+
+    void RTreeTest::tearDown() {
+    }
+
+    // perfect split on feature 2
+    void RTreeTest::test_best_split_perfect() {
+        // odd numbers are feature 1, even are feature 2
+        const std::vector<float> xs {
+            1.2f, 12.2f,
+            6.4f, 2.6f,
+            1.9f, 12.2f,
+            6.2f, 2.6f
+        };
+        const std::vector<float> ys {
+            8.0f,
+            2.5f,
+            8.0f,
+            2.5f
+        };
+        const size_t nfeatures = 2;
+
+        const RTree tree(nfeatures, xs, ys);
+        auto split = tree.best_split();
+        auto col = split.first;
+        auto value = split.second;
+
+        size_t expected_feature = 1;
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(value, 2.6f, m_tolerance);
+        CPPUNIT_ASSERT_EQUAL(col, expected_feature);
+    }
+}
