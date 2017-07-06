7dfe3c5b21b62dea7dcb389c597abe7d1821b65c
Krysta Bouzek <nwkrystab@gmail.com>
2017-07-05 23:11:52 -0700

    Updating R example to be a little more useful

--- src/rcpp_oddvibe.cpp
+++ src/rcpp_oddvibe.cpp
@@ -30,13 +30,13 @@ using DoubleVector = std::vector<double>;
 //' @examples
 //' tmp.seed <- 1480561820
 //' set.seed(tmp.seed)
-//' nrows <- 50
+//' nrows <- 500
 //' nfeatures <- 2
 //' intercept <- 0.75
 //' beta.one <- 2.0
 //' beta.two <- 5.8
 //' xnoise.one <- rnorm(nrows)
-//' xnoise.two <- rnorm(nrows)
+//' xnoise.two <- 10 * rnorm(nrows)
 //' threshold <- 0.7 * nrows
 //' x.threshold <- 2 * threshold
 //'
@@ -47,17 +47,15 @@ using DoubleVector = std::vector<double>;
 //' mat <- t(matrix(xs, nrow = 2))
 //' ys <- intercept + beta.one * mat[, 1] + beta.two * mat[, 2]
 //'
-//' # Generate some obvious outliers every 5 rows
-//' row.idx <- 1000 * (1:nrows)
-//' nonzeros <- seq(1, threshold, 5)
-//' row.idx[-nonzeros] <- 1
-//' ys <- ys * row.idx
+//' # Set a few of obvious outliers
+//' outlier.idx <- c(1, 11)
+//' ys[outlier.idx] <- 1000 * ys[outlier.idx]
 //'
 //' # add noise to avoid a boring perfect fit
 //' mat[, 1] <- mat[, 1] + xnoise.one
 //' mat[, 2] <- mat[, 2] + xnoise.two
 //'
-//' outliers <- FindOutlierWeights(mat, ys, 5000, tmp.seed)
+//' outliers <- FindOutlierWeights(mat, ys, 500, tmp.seed)
 //'
 //' # look at the top 6 possible outliers vs the "top" 6 unlikely outliers
 //' df <- cbind(as.data.frame(mat), data.frame(ys, outliers))
