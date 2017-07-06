54f1a94fb0c76eaff33bc1c1ba91fdf997ac4530
Krysta Bouzek <nwkrystab@gmail.com>
2017-07-05 23:20:46 -0700

    Updating python examples

--- py/example.py
+++ py/example.py
@@ -5,14 +5,14 @@ import random
 if __name__ == '__main__':
     tmp_seed = 1480561820
     random.seed(tmp_seed)
-    nrows = 50
+    nrows = 500
     nfeatures = 2
     intercept = 0.75
     beta_one = 2.0
     beta_two = 5.8
 
     xnoise_one = np.random.normal(0, 1, nrows)
-    xnoise_two = np.random.normal(0, 1, nrows)
+    xnoise_two = 10.0 * np.random.normal(0, 1, nrows)
     threshold = int(0.7 * nrows)
     x_threshold = int(2 * threshold)
 
@@ -26,16 +26,13 @@ if __name__ == '__main__':
 
     ys = intercept + beta_one * mat[:, 0] + beta_two * mat[:, 1]
 
-    # Generate some obvious outliers every 5 rows
-    row_idx = 1000 * np.array(list(xrange(1, nrows + 1)))
-    nonzeros = set(list(xrange(0, threshold, 5)))
-    zeros = [x for x in xrange(0, nrows) if x not in nonzeros]
-    row_idx[zeros] = 1
-    ys = ys * row_idx
+    # Set a few of obvious outliers
+    outlier_idx = [1, 11]
+    ys[outlier_idx] = 1000 * ys[outlier_idx]
 
     # add noise to avoid a boring perfect fit
     mat[:, 0] = mat[:, 0] + xnoise_one
     mat[:, 1] = mat[:, 1] + xnoise_two
 
     booster = PyBooster(tmp_seed)
-    weights = booster.find_outlier_weights(mat, ys, 5000)
+    weights = booster.find_outlier_weights(mat, ys, 500)
